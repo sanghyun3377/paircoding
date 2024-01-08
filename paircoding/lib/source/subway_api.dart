0e3caf02bee8cea8f8f83a5b33cf9148f2418d9c
OSS0302 <tkdtn3264@gmail.com>
2024-01-08 17:34:40 +0900

    api  수정

--- paircoding/lib/source/subway_api.dart
+++ paircoding/lib/source/subway_api.dart
@@ -4,14 +4,13 @@ import 'package:paircoding/model/subway_model.dart';
 import 'package:http/http.dart' as http;
 
 class SubWayApi {
-  Future<SubWayModel> getSubwayData(String stationName) async {
+  Future<List<SubWayModel>> getSubwayData(String stationName) async {
     var uri =
         'http://swopenapi.seoul.go.kr/api/subway/sample/json/realtimeStationArrival/0/5/$stationName';
     var url = Uri.parse(uri);
     var source = await http.get(url);
-    var json = jsonDecode(source.body);
-    var data = SubWayModel.fromJson(json["realtimeArrivalList"]);
-    print(data);
-    return data;
+    var json = jsonDecode(source.body) as List<dynamic>;
+    pri
+   return json.map((e) => SubWayModel.fromJson(e)).toList();
   }
 }
